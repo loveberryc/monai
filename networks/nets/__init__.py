9869b37159e320b1dc850596c216707b6794789d
Kaiyu <82535927+loveberryc@users.noreply.github.com>
2023-05-23 19:43:52 +0800

    Update __init__.py

--- networks/nets/__init__.py
+++ networks/nets/__init__.py
@@ -81,7 +81,7 @@ from .senet import (
     seresnext50,
     seresnext101,
 )
-from .swin_unetr import PatchMerging, PatchMergingV2, SwinUNETR,SwinUNETR_Muti,SwinUNETR_Rec,SwinUNETR_RecDS
+from .swin_unetr import PatchMerging, PatchMergingV2, SwinUNETR,SwinUNETR_Muti,SwinUNETR_Rec,SwinUNETR_New
 from .torchvision_fc import TorchVisionFCModel
 from .transchex import BertAttention, BertMixedLayer, BertOutput, BertPreTrainedModel, MultiModal, Pooler, Transchex
 from .unet import UNet, Unet
